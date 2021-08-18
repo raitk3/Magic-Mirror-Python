e577d45a3d8aaf81f5572c8de5716001d24d9978
Rait Kulbok <rakulb@ttu.ee>
2021-08-18 20:42:49 +0300

    Font update

--- MMP.py
+++ MMP.py
@@ -11,7 +11,8 @@ from tkinter import *
 LOCALE = "et_EE.utf8"
 
 # FONT
-FONT = 'caviar dreams'
+FONT = 'Poppins'
+BOLD_FONT = 'Poppins Bold'
 # CLOCK
 CLOCK_FONT_SIZE = 72
 DATE_FONT_SIZE = 25
@@ -35,7 +36,7 @@ TROLLEY_COLOUR = "#3466B1"
 
 
 class BusController:
-    def __init__(self, coords, root = None, rowspan=1, colspan=1):
+    def __init__(self, coords, root=None, rowspan=1, colspan=1):
         self.schedule = []
         self.root = root
         self.last_updated = None
@@ -45,7 +46,7 @@ class BusController:
         self.number_frames = []
         self.terminus_frames = []
         self.time_frames = []
-        
+
         self.create_bus_frames()
 
     def update_schedule(self):
@@ -76,21 +77,23 @@ class BusController:
         if self.root != None:
             for i in range(3):
                 number_frame = tk.Label(self.root, text=f"Bus{i}", font=(
-                    FONT, BUS_NUMBER_SIZE), bg=DEFAULT_BACKGROUND_COLOUR, fg=DEFAULT_FONT_COLOUR)
-                number_frame.grid(row=self.coords[0] + self.rowspan - 1 - 2*i, column=self.coords[1], rowspan=2, sticky="NEWS")
+                    BOLD_FONT, BUS_NUMBER_SIZE), bg=DEFAULT_BACKGROUND_COLOUR, fg=DEFAULT_FONT_COLOUR)
+                number_frame.grid(
+                    row=self.coords[0] + self.rowspan - 1 - 2*i, column=self.coords[1], rowspan=2, sticky="NEWS")
                 self.number_frames.append(number_frame)
 
                 terminus_frame = tk.Label(self.root, text=f"Terminus{i}", font=(
                     FONT, BUS_LITTLE_FONT_SIZE), bg=DEFAULT_BACKGROUND_COLOUR, fg=DEFAULT_FONT_COLOUR, anchor="sw")
-                terminus_frame.grid(row=self.coords[0] + self.rowspan - 1 - 2*i, column=self.coords[1]+1, columnspan=self.colspan - 1, sticky="NEWS")
+                terminus_frame.grid(row=self.coords[0] + self.rowspan - 1 - 2*i,
+                                    column=self.coords[1]+1, columnspan=self.colspan - 1, sticky="NEWS")
                 self.terminus_frames.append(terminus_frame)
 
                 time_frame = tk.Label(self.root, text=f"Time{i}", font=(
                     FONT, BUS_LITTLE_FONT_SIZE), bg=DEFAULT_BACKGROUND_COLOUR, fg=DEFAULT_ACCENT_COLOUR, anchor="nw")
-                time_frame.grid(row=self.coords[0] + self.rowspan - 2*i, column=self.coords[1]+1, columnspan=self.colspan - 1, sticky="NEWS")
+                time_frame.grid(row=self.coords[0] + self.rowspan - 2*i,
+                                column=self.coords[1]+1, columnspan=self.colspan - 1, sticky="NEWS")
                 self.time_frames.append(time_frame)
 
-    
     def update(self, timeController, force_update=False):
         updated = False
         if force_update or len(self.schedule) < 3 or self.last_updated == None or self.schedule[0][2] < timeController.current_time or timeController - self.last_updated > 300:
@@ -104,13 +107,15 @@ class BusController:
             for i in range(3):
                 row = 2-i
                 if len(self.schedule) > i:
-                    self.number_frames[row].configure(text=self.schedule[i][0], fg=BUS_COLOUR)
+                    self.number_frames[row].configure(
+                        text=self.schedule[i][0], fg=BUS_COLOUR)
                     if self.schedule[i][3] == "trol":
                         self.number_frames[row].configure(fg=TROLLEY_COLOUR)
-                    self.terminus_frames[row].configure(text=self.schedule[i][1])
+                    self.terminus_frames[row].configure(
+                        text=self.schedule[i][1])
                     self.time_frames[row].configure(
                         text=f"{self.get_time_remaining_string(self.schedule[i][2], timeController.time_in_seconds)} ({timeController.seconds_to_time(self.schedule[i][2])})"
-                )
+                    )
 
                 else:
                     self.number_frames[row].configure(text="")
@@ -119,7 +124,7 @@ class BusController:
 
 
 class WeatherController:
-    def __init__(self, coords, root = None):
+    def __init__(self, coords, root=None):
         self.coords = coords
         self.root = root
         self.data = []
@@ -146,10 +151,10 @@ class WeatherController:
                 wind_speed = response["wind"]["speed"]
                 last_updated = timeController.current_time
                 self.data = [temperature,
-                            feels_like,
-                            tk.PhotoImage(data=weather_icon),
-                            wind_direction,
-                            wind_speed]
+                             feels_like,
+                             tk.PhotoImage(data=weather_icon),
+                             wind_direction,
+                             wind_speed]
         except Exception:
             print("Failed to update weather!")
 
@@ -158,7 +163,6 @@ class WeatherController:
         angle_of_attack = (angle + 22.5) % 360
         return sectors[int(angle_of_attack // 45)]
 
-
     def convert_K_to_C(self, temperature_in_kelvin):
         return temperature_in_kelvin - 273.15
 
@@ -166,29 +170,34 @@ class WeatherController:
         if self.root != None:
             temp_frame = tk.Label(self.root, text=f"Temperature", font=(
                 FONT, TEMP_SIZE), bg=DEFAULT_BACKGROUND_COLOUR, fg=DEFAULT_FONT_COLOUR, anchor="se")
-            temp_frame.grid(row=self.coords[0]+1, column=self.coords[1]+1, columnspan=3, sticky="NEWS")
+            temp_frame.grid(
+                row=self.coords[0]+1, column=self.coords[1]+1, columnspan=3, sticky="NEWS")
             self.widgets.append(temp_frame)
 
             feels_frame = tk.Label(self.root, text=f"Temperature", font=(
                 FONT, FEELS_LIKE_SIZE), bg=DEFAULT_BACKGROUND_COLOUR, fg=DEFAULT_ACCENT_COLOUR, anchor="ne")
-            feels_frame.grid(row=self.coords[0]+2, column=self.coords[1]+2, columnspan=2, sticky="NEWS")
+            feels_frame.grid(
+                row=self.coords[0]+2, column=self.coords[1]+2, columnspan=2, sticky="NEWS")
             self.widgets.append(feels_frame)
 
             icon_frame = tk.Label(
                 self.root, text="Icon", bg=DEFAULT_BACKGROUND_COLOUR, fg=DEFAULT_ACCENT_COLOUR, anchor="e")
-            icon_frame.grid(row=self.coords[0]+1, column=self.coords[1], sticky="NEWS")
+            icon_frame.grid(row=self.coords[0]+1,
+                            column=self.coords[1], sticky="NEWS")
             self.widgets.append(icon_frame)
 
             wind_direction_frame = tk.Label(
                 self.root, text="Wind", bg=DEFAULT_BACKGROUND_COLOUR, fg=DEFAULT_ACCENT_COLOUR,
                 font=(FONT, WIND_SIZE), anchor="e")
-            wind_direction_frame.grid(row=self.coords[0], column=self.coords[1]+2, columnspan=2, sticky="NEWS")
+            wind_direction_frame.grid(
+                row=self.coords[0], column=self.coords[1]+2, columnspan=2, sticky="NEWS")
             self.widgets.append(wind_direction_frame)
 
             wind_speed_frame = tk.Label(
                 self.root, text="Wind", bg=DEFAULT_BACKGROUND_COLOUR, fg=DEFAULT_ACCENT_COLOUR,
                 font=(FONT, WIND_SIZE), anchor="e")
-            wind_speed_frame.grid(row=self.coords[0], column=self.coords[1]+1, columnspan=2, sticky="NEWS")
+            wind_speed_frame.grid(
+                row=self.coords[0], column=self.coords[1]+1, columnspan=2, sticky="NEWS")
             self.widgets.append(wind_speed_frame)
 
     def update(self, timeController):
@@ -199,15 +208,17 @@ class WeatherController:
                     if self.data[i] != "N/A":
                         self.widgets[i].configure(image=self.data[i])
                 elif i == 3:
-                    self.widgets[i].configure(text=self.convert_angle_to_dir(self.data[i]))
+                    self.widgets[i].configure(
+                        text=self.convert_angle_to_dir(self.data[i]))
                 elif i == 4:
-                    self.widgets[i].configure(text=f"{round(self.data[i], 1)}m/s")
+                    self.widgets[i].configure(
+                        text=f"{round(self.data[i], 1)}m/s")
                 else:
                     self.widgets[i].configure(text=self.data[i])
 
 
 class TimeController:
-    def __init__(self, coords, root = None, rowspan=1, colspan=1):
+    def __init__(self, coords, root=None, rowspan=1, colspan=1):
         self.coords = coords
         self.rowspan = rowspan
         self.colspan = colspan
@@ -228,7 +239,7 @@ class TimeController:
         self.time_in_seconds = (now - midnight).seconds
         if self.widget != None:
             self.widget.configure(text=self.time_as_string)
-        
+
     def seconds_to_time(self, seconds):
         hours = seconds // 3600
         minutes = int((seconds / 60) % 60)
@@ -236,13 +247,14 @@ class TimeController:
 
     def create_clock_widget(self):
         if self.root != None:
-            self.widget = tk.Label(self.root, font=(FONT, CLOCK_FONT_SIZE),
-                            bg=DEFAULT_BACKGROUND_COLOUR, fg=DEFAULT_FONT_COLOUR, anchor="nw")
-            self.widget.grid(row=self.coords[0], column=self.coords[1], rowspan=self.rowspan, columnspan=self.colspan, sticky="NEWS")
+            self.widget = tk.Label(self.root, font=(BOLD_FONT, CLOCK_FONT_SIZE),
+                                   bg=DEFAULT_BACKGROUND_COLOUR, fg=DEFAULT_FONT_COLOUR, anchor="nw")
+            self.widget.grid(row=self.coords[0], column=self.coords[1],
+                             rowspan=self.rowspan, columnspan=self.colspan, sticky="NEWS")
 
 
 class DateController:
-    def __init__(self, coords, root = None, rowspan=1, colspan=1):
+    def __init__(self, coords, root=None, rowspan=1, colspan=1):
         self.coords = coords
         self.rowspan = rowspan
         self.colspan = colspan
@@ -260,15 +272,15 @@ class DateController:
     def create_date_frame(self):
         if self.root != None:
             self.widget = tk.Label(self.root, font=(FONT, DATE_FONT_SIZE),
-                                bg=DEFAULT_BACKGROUND_COLOUR, fg=DEFAULT_FONT_COLOUR, anchor="nw")
-            self.widget.grid(row = self.coords[0]+1, column=self.coords[1], rowspan=self.rowspan, columnspan=self.colspan, sticky="NEWS")
+                                   bg=DEFAULT_BACKGROUND_COLOUR, fg=DEFAULT_FONT_COLOUR, anchor="nw")
+            self.widget.grid(row=self.coords[0]+1, column=self.coords[1],
+                             rowspan=self.rowspan, columnspan=self.colspan, sticky="NEWS")
 
 
 class Program:
     def __init__(self):
         locale.setlocale(locale.LC_TIME, LOCALE)
 
-        
         self.root = tk.Tk()
         self.root.title('Mirror')
         self.root.attributes("-fullscreen", True)
@@ -278,20 +290,23 @@ class Program:
             if i < 10:
                 self.root.rowconfigure(i, weight=1, uniform="row")
 
-        
-        #self.do_grid()
-        
-        self.timeController = TimeController(coords=(0, 0), root=self.root, rowspan=2, colspan=10)
-        self.dateController = DateController(coords=(1, 0), root=self.root, rowspan=1, colspan=10)
-        self.busController = BusController(coords=(5,0), root=self.root, rowspan=6, colspan=8)
-        self.weatherController = WeatherController(coords=(0,12), root=self.root)
+        # self.do_grid()
+
+        self.timeController = TimeController(
+            coords=(0, 0), root=self.root, rowspan=2, colspan=10)
+        self.dateController = DateController(
+            coords=(1, 0), root=self.root, rowspan=1, colspan=10)
+        self.busController = BusController(
+            coords=(5, 0), root=self.root, rowspan=6, colspan=8)
+        self.weatherController = WeatherController(
+            coords=(0, 12), root=self.root)
 
     def do_grid(self):
         for i in range(16):
             for j in range(10):
                 tk.Label(self.root, relief="raised").grid(
                     row=j, column=i, sticky="NEWS")
-    
+
     def mainloop(self):
         while True:
             self.timeController.update()
@@ -299,6 +314,7 @@ class Program:
             self.busController.update(self.timeController, False)
             self.weatherController.update(self.timeController)
             self.root.update()
+
 
 if __name__ == '__main__':
     Program().mainloop()
